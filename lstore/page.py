3242017a4b98155ed897124c2a476f938b2218c3
Zhongyi "William" Qiu <130611759+williamQ96@users.noreply.github.com>
2024-10-14 12:52:42 -0700

    page.py updated

--- lstore/page.py
+++ lstore/page.py
@@ -7,9 +7,15 @@ class Page:
         self.data = bytearray(config.PAGE_SIZE)
 
     def has_capacity(self):
-        pass
+        record_size = 256
+        return len(self.data) - self.num_records * record_size >= record_size
 
     def write(self, value):
+        record_size = 256
+        start_index = self.num_records * record_size
+        self.data[start_index:start_index + record_size] = value.to_bytes(record_size, byteorder='big')
         self.num_records += 1
-        pass
+        else:
+        # If the page is full, raise an exception
+        raise Exception("Page is full. Cannot write more records.")
 
