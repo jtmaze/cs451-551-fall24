3d852d42c3f9913192c3b3bc01a3d9d9397e8822
Zhongyi "William" Qiu <130611759+williamQ96@users.noreply.github.com>
2024-10-16 10:26:30 -0700

    db.py updated

--- lstore/db.py
+++ lstore/db.py
@@ -14,6 +14,13 @@ class Database():
     def close(self):
         pass
 
+    """
+    # Creates a new table
+    :param name: string         #Table name
+    :param num_columns: int     #Number of Columns: all columns are integer
+    :param key: int             #Index of table key in columns
+    """
+
     def create_table(self, name, num_columns, key_index):
         """
         # Creates a new table
@@ -22,16 +29,31 @@ class Database():
         :param key: int             #Index of table key in columns
         """
         table = Table(name, num_columns, key_index)
+        self.tables[name] = table  # Add the table to the database's table dictionary.
         return table
+
+    """
+    # Deletes the specified table
+    """
 
     def drop_table(self, name):
         """
         # Deletes the specified table
         """
-        pass
+        if name in self.tables:
+            del self.tables[name]
+        else:
+            raise Exception(f"Table '{name}' does not exist.")
+
+    """
+    # Returns table with the passed name
+    """
 
     def get_table(self, name):
         """
         # Returns table with the passed name
         """
-        pass
+        if name in self.tables:
+            return self.tables[name]
+        else:
+            raise Exception(f"Table '{name}' does not exist.")
