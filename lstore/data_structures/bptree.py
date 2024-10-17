92fb1852787fe0f3ebecf9f31907250311fb2bc4
James Maze <jmaze@uoregon.edu>
2024-10-17 14:41:10 -0700

    Just contemplating b-tree implementation. Coding it up tommorrow

--- lstore/data_structures/bptree.py
+++ lstore/data_structures/bptree.py
@@ -1,3 +1,30 @@
 class BPTree:
-    def __init__(self):
+    def __init__(self, n, keys, values):
+
+        self.n = n
+        self.keys = keys
+        self.values = values
+        
+    def point_query(self, key):
+
+        value = 'somestuff'
+
+        return value
+
+    def range_query(self, low, high):
+        
+        # Is it appropriate to return a list???
+        values = ['some other stuff']
+
+        return values
+    
+    def insert(self, key, value):
+
+        print('Key(s) inserted')
         pass
+
+    def delete(self, key):
+
+        print('Key(s) deleted')
+        pass
+
