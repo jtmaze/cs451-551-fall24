f7b31e16b895a2b299d7750e6f3d502ceab8a045
Eric Zander <60997515+ericzander@users.noreply.github.com>
2024-10-17 23:11:25 -0700

    Converted buffer pages to OrderedDict for easy eviction of least recently used pages

--- lstore/storage/bufferpool.py
+++ lstore/storage/bufferpool.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 class Bufferpool:
     """
     A simple bufferpool that uses a hash table to store pages in memory,
@@ -6,23 +8,31 @@ class Bufferpool:
     def __init__(self, max_size=None):
         # Max buffer size (optional), could be useful for future use in eviction policy
         self.max_size = max_size
-        self.page_directory = {}
+        self.pages = OrderedDict()
 
     def get_page(self, rid):
         """
         Retrieves a page using the given RID from the bufferpool.
         """
-        return self.page_directory.get(rid, None)
+        return self.pages.get(rid, None)
 
     def add_page(self, rid, page):
         """
         Adds a page to the bufferpool and associates it with the given RID.
         """
-        self.page_directory[rid] = page
+        # If page already in buffer, move to end of queue
+        if rid in self.pages:
+            self.pages.move_to_end(rid)
+
+        self.pages[rid] = page
+
+        # Remove oldest if buffer full (first in ordered dict)
+        if len(self.pages) > self.max_size:
+            self.pages.popitem(last=False)
 
     def remove_page(self, rid):
         """
         Removes a page from the bufferpool.
         """
-        if rid in self.page_directory:
-            del self.page_directory[rid]
+        if rid in self.pages:
+            del self.pages[rid]
