11527c9942f423a74d086efb4148c3ecfbd3f88e
Eric Zander <60997515+ericzander@users.noreply.github.com>
2024-11-13 13:03:15 -0800

    Bits, not bytes

--- lstore/config.py
+++ lstore/config.py
@@ -1,5 +1,5 @@
 PAGE_SIZE = 4096
-RECORD_SIZE = 128
+RECORD_SIZE = 128 // 8
 MAX_BUFFER_PAGES = None   # Maximum number of pages in buffer (None -> uncapped)
 PRINT_ERRORS = True
 DEBUG_PRINT = True
